8ef731a1d1b49858914f176d79e6a45016490ed3
leanhtai01 <leanhtai01@gmail.com>
2021-08-05 13:59:34 +0700

    add script install core packages

---
+++ install_core_packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+install_command="apt update -y && apt full-upgrade -y && apt install -y"
+install_command_backports="apt update -y && apt full-upgrade -y && apt install -y -t buster-backports"
+
+# fonts
+$install_command fonts-hack
+
+# tools
+$install_command lm-sensors
+
+# programming
+$install_command llvm clang npm build-essential
+
+# editor
+$install_command_backports emacs dictionaries-common
