07e6ad6a5794cb9ea0dc5cc64a816dde6ac71592
leanhtai01 <leanhtai01@gmail.com>
2021-08-05 15:18:45 +0700

    add VAAPI driver

--- install_drivers.sh
+++ install_drivers.sh
@@ -11,3 +11,6 @@ $install_command firmware-iwlwifi intel-microcode firmware-realtek firmware-misc
 
 # ensure Vulkan and 32-bit support
 $install_command mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
+
+# VAAPI driver
+$install_command intel-media-va-driver
