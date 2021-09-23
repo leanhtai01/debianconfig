5ff0b871b11ec2265c2fb3757558f28782beb14a
leanhtai01 <leanhtai01@gmail.com>
2021-09-23 17:57:51 +0700

    install tlp: move to bullseye

--- install_tlp.sh
+++ install_tlp.sh
@@ -3,7 +3,7 @@
 set -e
 
 install_command="apt install -y"
-install_command_backports="apt install -y -t buster-backports"
+install_command_backports="apt install -y -t bullseye-backports"
 
-$install_command_backports tlp
+$install_command tlp
 $install_command acpi-call-dkms
