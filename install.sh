efe4619795b5bfb47b135e2547b6cd7d1c945bbc
leanhtai01 <leanhtai01@gmail.com>
2021-09-24 16:28:10 +0700

    install gh, sublime tools and LAMP stack

--- install.sh
+++ install.sh
@@ -9,3 +9,6 @@ $current_dir/enable_multi_arch.sh
 $current_dir/install_drivers.sh
 $current_dir/install_core_packages.sh
 $current_dir/install_tlp.sh
+$current_dir/install_gh.sh
+$current_dir/install_sublime_tools.sh
+$current_dir/install_LAMP.sh
