79e20c59d434b9fa3d9a22e360ac50c39206126a
leanhtai01 <leanhtai01@gmail.com>
2021-09-24 17:17:23 +0700

    install curl before use

--- install_gh.sh
+++ install_gh.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+install_command="sudo apt install -y"
+install_command_backports="sudo apt install -y -t bullseye-backports"
+
+# install curl before use
+$install_command curl
+
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
