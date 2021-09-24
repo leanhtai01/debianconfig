6fc3a3546e666206d727ae32f5997b59facb17a3
leanhtai01 <leanhtai01@gmail.com>
2021-09-24 18:43:38 +0700

    add script install virtualbox

---
+++ install_virtualbox.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+install_command="sudo apt install -y"
+install_command_backports="sudo apt install -y -t bullseye-backports"
+
+# import fasttrack archive keyring
+$install_command fasttrack-archive-keyring
+
+# setup fasttrack repositories
+printf "\n# fasttrack repositories\n" | sudo tee /etc/apt/sources.list
+printf "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib\n" | sudo tee /etc/apt/sources.list
+printf "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib\n" | sudo tee /etc/apt/sources.list
+
+# install virtualbox
+$install_command virtualbox virtualbox-ext-pack
