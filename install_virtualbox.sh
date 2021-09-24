f1f15de4fcd9c96e9774fb477b436b1054048170
leanhtai01 <leanhtai01@gmail.com>
2021-09-24 18:54:20 +0700

    fix fatal error cause sources.list overwrited

--- install_virtualbox.sh
+++ install_virtualbox.sh
@@ -9,9 +9,9 @@ install_command_backports="sudo apt install -y -t bullseye-backports"
 $install_command fasttrack-archive-keyring
 
 # setup fasttrack repositories
-printf "\n# fasttrack repositories\n" | sudo tee /etc/apt/sources.list
-printf "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib\n" | sudo tee /etc/apt/sources.list
-printf "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib\n" | sudo tee /etc/apt/sources.list
+printf "\n# fasttrack repositories\n" | sudo tee -a /etc/apt/sources.list
+printf "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib\n" | sudo tee -a /etc/apt/sources.list
+printf "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib\n" | sudo tee -a /etc/apt/sources.list
 
 # install virtualbox
 $install_command virtualbox virtualbox-ext-pack
