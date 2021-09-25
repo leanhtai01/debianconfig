eb6ba450da1a0655105f5b0fee8fd873f0e7ac3b
leanhtai01 <leanhtai01@gmail.com>
2021-09-25 09:26:06 +0700

    add script install ibus-bamboo

---
+++ install_ibus-bamboo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+install_command="sudo apt install -y"
+install_command_backports="sudo apt install -y -t bullseye-backports"
+
+# update system
+sudo apt update && sudo apt full-upgrade -y
+
+# install curl before use
+$install_command curl
+
+echo 'deb http://download.opensuse.org/repositories/home:/lamlng/Debian_10/ /' | sudo tee /etc/apt/sources.list.d/home:lamlng.list
+curl -fsSL https://download.opensuse.org/repositories/home:lamlng/Debian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_lamlng.gpg > /dev/null
+sudo apt update
+sudo apt install ibus-bamboo
