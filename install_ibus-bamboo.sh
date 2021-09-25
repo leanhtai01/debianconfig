a384f07c3718627ca46d32fecc526de4a92f22bd
leanhtai01 <leanhtai01@gmail.com>
2021-09-25 09:27:46 +0700

    no install confirm

--- install_ibus-bamboo.sh
+++ install_ibus-bamboo.sh
@@ -14,4 +14,4 @@ $install_command curl
 echo 'deb http://download.opensuse.org/repositories/home:/lamlng/Debian_10/ /' | sudo tee /etc/apt/sources.list.d/home:lamlng.list
 curl -fsSL https://download.opensuse.org/repositories/home:lamlng/Debian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_lamlng.gpg > /dev/null
 sudo apt update
-sudo apt install ibus-bamboo
+$install_command ibus-bamboo
