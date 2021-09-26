bbe93df6f8e6f577aa022051cff0148df732e95d
leanhtai01 <leanhtai01@gmail.com>
2021-09-26 11:36:13 +0700

    fix command install local .deb file

--- install_google-chrome.sh
+++ install_google-chrome.sh
@@ -9,4 +9,4 @@ install_command_backports="sudo apt install -y -t bullseye-backports"
 sudo apt update && sudo apt full-upgrade -y
 
 curl -LJO https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -y google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
