b971eb019f57e5390f13a41b685b507c585276e2
leanhtai01 <leanhtai01@gmail.com>
2021-09-26 08:26:35 +0700

    ibus bamboo update repo to debian 11

--- install_ibus-bamboo.sh
+++ install_ibus-bamboo.sh
@@ -11,8 +11,8 @@ sudo apt update && sudo apt full-upgrade -y
 # install curl before use
 $install_command curl
 
-echo 'deb http://download.opensuse.org/repositories/home:/lamlng/Debian_10/ /' | sudo tee /etc/apt/sources.list.d/home:lamlng.list
-curl -fsSL https://download.opensuse.org/repositories/home:lamlng/Debian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_lamlng.gpg > /dev/null
+echo 'deb http://download.opensuse.org/repositories/home:/lamlng/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/home:lamlng.list
+curl -fsSL https://download.opensuse.org/repositories/home:lamlng/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_lamlng.gpg > /dev/null
 sudo apt update
 $install_command ibus-bamboo
 
