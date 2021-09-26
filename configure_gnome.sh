f3789070eedf25ebf3e4730e0b8cf2be796a5e50
leanhtai01 <leanhtai01@gmail.com>
2021-09-26 10:15:54 +0700

    add script configure GNOME

---
+++ configure_gnome.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+set -e
+
+# swap Ctrl - CapsLock
+# gsettings set org.gnome.desktop.input-sources xkb-options "['lv3:rwin_switch', 'ctrl:swapcaps']"
+
+# swap CapsLock - Esc
+# gsettings set org.gnome.desktop.input-sources xkb-options "['lv3:rwin_switch', 'caps:swapescape']"
+
+# set applications theme to dark
+gsettings set org.gnome.desktop.interface gtk-theme "'Adwaita-dark'"
+
+# # set alternate characters key
+# gsettings set org.gnome.desktop.input-sources xkb-options "['lv3:rwin_switch']"
+
+# show battery percentage
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+
+# # disable touchpad
+# gsettings set org.gnome.desktop.peripherals.touchpad send-events "'disabled'"
+
+# disable two-finger-scrolling touchpad
+gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled true
+
+# enable edge-scrolling touchpad
+gsettings set org.gnome.desktop.peripherals.touchpad edge-scrolling-enabled false
+
+# enable touchpad tap-to-click
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+
+# set touchpad speed
+gsettings set org.gnome.desktop.peripherals.touchpad speed 0.51470588235294112
+
+# enable Night Light
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 18.0
+
+# show weekday
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+
+# show date
+gsettings set org.gnome.desktop.interface clock-show-date true
+
+# # change fonts to Hack
+# gsettings set org.gnome.desktop.interface font-name "'Hack 10'"
+# gsettings set org.gnome.desktop.interface document-font-name "'Hack 11'"
+# gsettings set org.gnome.desktop.interface monospace-font-name "'Hack 10'"
+# gsettings set org.gnome.desktop.wm.preferences titlebar-font "'Hack Bold 11'"
+
+# show week number
+gsettings set org.gnome.desktop.calendar show-weekdate true
+
+# disable suspend
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type "'nothing'"
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type "'nothing'"
+
+# set cursor theme
+# gsettings set org.gnome.desktop.interface cursor-theme "'ArchCursorTheme'"
+
+# set default folder viewer nautilus
+gsettings set org.gnome.nautilus.preferences default-folder-viewer "'list-view'"
+
+# set default-zoom-level
+gsettings set org.gnome.nautilus.list-view default-zoom-level "'large'"
+
+# empty favorite-apps
+gsettings set org.gnome.shell favorite-apps "[]"
+
+# # setup favorite-apps
+# gsettings set org.gnome.shell favorite-apps "['torbrowser.desktop', 'thunderbird.desktop', 'org.gnome.Evince.desktop', 'calibre-gui.desktop', 'com.github.johnfactotum.Foliate.desktop', 'org.keepassxc.KeePassXC.desktop', 'virt-manager.desktop', 'virtualbox.desktop', 'sublime_merge.desktop', 'org.gnome.gedit.desktop']"
+
+# switch applications only in current workspace
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+
+# # disable auto-update GNOME
+# gsettings set org.gnome.software download-updates false
+# gsettings set org.gnome.software download-updates-notify false
+
+# set nautilus initial-size
+gsettings set org.gnome.nautilus.window-state initial-size "(1169, 785)"
+
+# sort directories first
+# gsettings set org.gtk.settings.file-chooser sort-directories-first true
+
+# switch windows
+# gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
+# gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
+# gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+# gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
---
+++ make_shortcuts_gnome.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+SCHEMATOLIST="org.gnome.settings-daemon.plugins.media-keys"
+SCHEMATOITEM="org.gnome.settings-daemon.plugins.media-keys.custom-keybinding"
+PATHTOCUSTOMKEY="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom"
+
+gsettings set $SCHEMATOLIST custom-keybindings "['${PATHTOCUSTOMKEY}0/', '${PATHTOCUSTOMKEY}1/', '${PATHTOCUSTOMKEY}2/', '${PATHTOCUSTOMKEY}3/', '${PATHTOCUSTOMKEY}4/', '${PATHTOCUSTOMKEY}5/', '${PATHTOCUSTOMKEY}6/', '${PATHTOCUSTOMKEY}7/']"
+
+# open Emacs
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}0/ name "'Emacs'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}0/ binding "'<Primary><Alt>e'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}0/ command "'emacs'"
+
+# # open Chromium
+# gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ name "'Chromium'"
+# gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ binding "'<Primary><Alt>c'"
+# gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ command "'chromium'"
+
+# open Google Chrome
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ name "'Google Chrome'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ binding "'<Primary><Alt>c'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}1/ command "'google-chrome-stable'"
+
+# open GNOME Terminal
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ name "'GNOME Terminal'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ binding "'<Primary><Alt>t'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}2/ command "'gnome-terminal'"
+
+# open Nautilus
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}3/ name "'Nautilus'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}3/ binding "'<Super>e'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}3/ command "'nautilus'"
+
+# open Krusader
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}4/ name "'Krusader'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}4/ binding "'<Primary><Alt>k'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}4/ command "'krusader'"
+
+# open GNOME Terminal - fish shell
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}5/ name "'GNOME Terminal - fish shell'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}5/ binding "'<Primary><Alt>r'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}5/ command "'gnome-terminal -e fish'"
+
+# open Firefox Developer Edition
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}6/ name "'Firefox Developer Edition'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}6/ binding "'<Primary><Alt>f'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}6/ command "'firefox-developer-edition'"
+
+# open GVim
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}7/ name "'GVim'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}7/ binding "'<Primary><Alt>v'"
+gsettings set $SCHEMATOITEM:${PATHTOCUSTOMKEY}7/ command "'gvim'"
